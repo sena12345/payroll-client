e581147b51c9d8ccbb2123f8ec14e4a412c97b66
alwise <kemevoralwise@gmail.com>
2021-01-20 12:34:02 +0000

    updated

--- src/Views/dashboard/RegisterUser.js
+++ src/Views/dashboard/RegisterUser.js
@@ -1,82 +1,99 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import 'react-bootstrap';
-import {Container, Form, Row, InputGroup, FormControl, Button, Col} from 'react-bootstrap';
+import { Container, Form, Row, InputGroup, FormControl, Button, Col } from 'react-bootstrap';
+import Config from '../../data-operations/data-queries/config';
+import { Roles, Department, Position, Designation } from '../../data-operations/_sahred/models';
+import { useAuth } from '../../_services/auth-context';
 
-function RegisterUser() { 
-    return (
-        <div className="registeruser">
-            <Container>
-                
-                    <Form className="register-form">
-                                               
-                        <Form.Group  controlId="empFullName">
-                            <Form.Label>Full Name</Form.Label>
-                            <Form.Control type="text" placeholder="Enter Employee Full Name" />
-                        </Form.Group>
+function RegisterUser() {
+	const { currentUser } = useAuth();
 
-                        <Form.Group controlId="empEmail">
-                            <Form.Label>Email</Form.Label>
-                            <Form.Control type="text" placeholder="Enter Employee Email" />
-                        </Form.Group>
-                        
+	const [ roles, setRoles ] = useState([ Roles ]);
+	const queriesStore = Config(currentUser);
 
-                        <Form.Group controlId="empID">
-                            <Form.Label>Employee ID</Form.Label>
-                            <Form.Control type="text" />
-                        </Form.Group>
+	async function fetchInits() {
+		queriesStore
+			.getDepartments()
+			.then((res) => {
+				setRoles(res.data);
+				console.log(res.data);
+			})
+			.catch((err) => {
+				console.log('cannot fetch roles due to: ' + err);
+			});
+	}
 
-                        <Form.Row>
-                            <Form.Group as={Col} controlId="empPosition">
-                            <Form.Label>Position</Form.Label>
-                            <Form.Control type="text" />
-                            </Form.Group>
+	useEffect(() => {
+		fetchInits();
+	}, []);
 
-                            <Form.Group as={Col} controlId="empDepartment">
-                            <Form.Label>Department</Form.Label>
-                            <Form.Control type="text" />
-                            </Form.Group> 
-                        </Form.Row>
-                        
-                         <Form.Row>   
-                        <Form.Group as={Col} controlId="empSalary">
-                            <Form.Label>Salary</Form.Label>
-                            <Form.Control type="text" />
-                        </Form.Group> 
-                        <Form.Group as={Col} controlId="empAllowance">
-                            <Form.Label>Allowance</Form.Label>
-                            <Form.Control type="text" />
-                            </Form.Group>
-                        </Form.Row>
-                        
-                        <Form.Group controlId="empDesignation">
-                            <Form.Label>Designation</Form.Label>
-                            <Form.Control type="text" />
-                        </Form.Group>
-                                                
-                        <Form.Row>
-                            <Form.Group as={Col} controlId="formGridState">
-                            <Form.Label>Roles</Form.Label>
-                            <Form.Control as="select" defaultValue="Choose...">
-                                <option>Choose...</option>
-                                    <option>Admin HR</option>
-                                    <option>Admin Accountant</option>
-                                    <option>Admin Super</option>
-                                    <option>Employee</option>
-                            </Form.Control>
-                            </Form.Group>
-                        </Form.Row>
+	return (
+		<div className="registeruser">
+			<Container>
+				<Form className="register-form">
+					<Form.Group controlId="empFullName">
+						<Form.Label>Full Name</Form.Label>
+						<Form.Control type="text" placeholder="Enter Employee Full Name" />
+					</Form.Group>
 
-                        <Button variant="primary" type="submit">
-                            Submit
-                        </Button>
-                               
-                    </Form>              
-                
-            </Container>     
-             
-        </div>
+					<Form.Group controlId="empEmail">
+						<Form.Label>Email</Form.Label>
+						<Form.Control type="text" placeholder="Enter Employee Email" />
+					</Form.Group>
 
-    );
+					<Form.Group controlId="empID">
+						<Form.Label>Employee ID</Form.Label>
+						<Form.Control type="text" />
+					</Form.Group>
+
+					<Form.Row>
+						<Form.Group as={Col} controlId="empPosition">
+							<Form.Label>Position</Form.Label>
+							<Form.Control type="text" />
+						</Form.Group>
+
+						<Form.Group as={Col} controlId="empDepartment">
+							<Form.Label>Department</Form.Label>
+							<Form.Control type="text" />
+						</Form.Group>
+					</Form.Row>
+
+					<Form.Row>
+						<Form.Group as={Col} controlId="empSalary">
+							<Form.Label>Salary</Form.Label>
+							<Form.Control type="text" />
+						</Form.Group>
+						<Form.Group as={Col} controlId="empAllowance">
+							<Form.Label>Allowance</Form.Label>
+							<Form.Control type="text" />
+						</Form.Group>
+					</Form.Row>
+
+					<Form.Group controlId="empDesignation">
+						<Form.Label>Designation</Form.Label>
+						<Form.Control type="text" />
+					</Form.Group>
+
+					<Form.Row>
+						<Form.Group as={Col} controlId="formGridState">
+							<Form.Label>Roles</Form.Label>
+							<Form.Control as="select" defaultValue="Choose...">
+								<option>Choose...</option>
+								<option>Admin HR</option>
+								<option>Admin Accountant</option>
+								<option>Admin Super</option>
+								<option>Employee</option>
+							</Form.Control>
+						</Form.Group>
+					</Form.Row>
+
+					<Button variant="primary" type="submit">
+						Submit
+					</Button>
+				</Form>
+			</Container>
+		</div>
+	);
 }
 
 export default RegisterUser;
