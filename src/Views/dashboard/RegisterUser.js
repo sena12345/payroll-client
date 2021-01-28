43dac601c34c06383e8a2f8e5fd6ed7c994883fd
JosephAbuanor <joseph.abuanor@amalitech.org>
2021-01-28 11:14:32 +0000

    Updated Register User Page Style

--- src/Views/dashboard/RegisterUser.js
+++ src/Views/dashboard/RegisterUser.js
@@ -129,7 +129,26 @@ function RegisterUser() {
   // 	});
   // 	setSortedDesignations(sortData);
   // };
-
+  const handleConfirm = (data) => {
+    showConfirmAlert({
+      title: "confirmation",
+      message: `Continue to add ${data.name} to employees ?`,
+      buttons: [
+        {
+          label: "No",
+          onClick: () => {
+            console.log("cancel");
+          },
+        },
+        {
+          label: "Yes",
+          onClick: () => {
+            onSubmit(data);
+          },
+        },
+      ],
+    });
+  };
   return loading ? (
     <MyLoader />
   ) : (
