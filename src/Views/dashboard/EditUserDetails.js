7046f7a7ce713a966b1931ee5cb65f41ee509cfd
alwise <kemevoralwise@gmail.com>
2021-01-31 07:17:23 +0000

    Update EditUserDetails.js

--- src/Views/dashboard/EditUserDetails.js
+++ src/Views/dashboard/EditUserDetails.js
@@ -99,9 +99,6 @@ function EditUserDetails() {
 	// };
 
 	const onSubmit = (data) => {
-		console.log(data.chkboxList);
-		return;
-
 		setLoading(true);
 		let rolesData = [];
 		rolesData.push(...employee.roles);
